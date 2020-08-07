14683d487527a7f772acef1efe3c6fc8ec5090b0
Fausto Llapa <faustollapa850@gmail.com>
2020-08-07 14:44:06 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -9,10 +9,10 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  letters = {a: 1, b: 2}
- 
-letters.min
- 
-alphabetical_order = {c: 100, d: 1}
-alphabetical_order.min
+  groceries = {
+   dairy: ["milk", "yogurt", "cheese"],
+   vegetable: ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"],
+   grains: ["rice", "pasta"]
+  }
 end
